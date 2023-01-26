18a0a7feabf1db501fdf90223deff0d58ceb8bf8
SeriousAntoxa <antoxa.podobed@gmail.com>
2023-01-26 19:20:50 +0300

    new proj

---
+++ js.js
@@ -0,0 +1,12 @@
+let sliderButton = document.querySelectorAll('.slider__control-dot_next')
+let sliderF = document.querySelector('.main__slider')
+let sliderS = document.querySelector('.main__slider2')
+
+sliderButton[0].addEventListener('click', (e) => {
+  sliderF.classList.remove('main__slider_vis')
+  sliderS.classList.add('main__slider_vis')
+})
+sliderButton[1].addEventListener('click', (e) => {
+  sliderF.classList.add('main__slider_vis')
+  sliderS.classList.remove('main__slider_vis')
+})
